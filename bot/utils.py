2a3838ac66afd0f6e2b7efa30fef51ac1a563355
Petrukovich Evgeny <evgeny.petrukovich@wecandevelopit.com>
2023-06-11 22:46:02 +0300

    fix typing

--- bot/utils.py
+++ bot/utils.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 from enum import IntEnum
-from typing import NewType, Generator
+from typing import NewType, Generator, Sequence, Callable
 from dataclasses import dataclass
 from collections import deque
 
@@ -28,19 +28,19 @@ class Graph:
         return len(self.nodes)
 
 
-def get_cycles(graph: Graph) -> Generator[list[int], None, None]:
+def get_cycles(graph: Graph) -> Generator[Sequence[GraphNode], None, None]:
     visited: list[VisitStatus] = [VisitStatus.NotVisited] * len(graph)
     visit_from: list[int] = [-1] * len(graph)
 
-    def restore_cycle(head_index, tail_index) -> list[int]:
+    def restore_cycle(head_index, tail_index) -> Sequence[GraphNode]:
         cycle = deque()
         curr_index = tail_index
         # unwinding cycle
         while curr_index != head_index:
-            cycle.append(curr_index)
+            cycle.appendleft(graph.nodes[curr_index])
             curr_index = visit_from[curr_index]
-        cycle.append(head_index)
-        return list(reversed(cycle))
+        cycle.appendleft(graph.nodes[head_index])
+        return cycle
 
     def bfs_search(curr_index, prev_index):
         if visited[curr_index] == VisitStatus.Visited:
@@ -52,6 +52,9 @@ def get_cycles(graph: Graph) -> Generator[list[int], None, None]:
         visited[curr_index] = VisitStatus.InProcessing
         visit_from[curr_index] = prev_index
         for next_index, edge_val in graph.nodes[curr_index].edges:
+            if next_index == prev_index:
+                # detect cycle with length == 2
+                continue
             yield from bfs_search(next_index, curr_index)
 
         visited[curr_index] = VisitStatus.NotVisited
@@ -63,24 +66,23 @@ if __name__ == '__main__':
 
     # example
     # 8 8
-    # 1 5
-    # 2 5
-    # 3 2
-    # 6 3
-    # 5 6
-    # 5 7
-    # 8 6
-    # 7 8
+    # 0 4
+    # 1 4
+    # 2 1
+    # 5 2
+    # 4 5
+    # 4 6
+    # 7 5
+    # 6 7
 
     n, m = map(int, input().split(' '))
     raw_edges: list[list[tuple]] = [[] for _ in range(n)]
     for _ in range(m):
         v1, v2 = map(int, input().split(' '))
-        raw_edges[v1 - 1].append((v2 - 1, 1))
+        raw_edges[v1].append((v2, 1))
 
     graph = Graph([
         GraphNode(index, e) for index, e in enumerate(raw_edges)
     ])
-    print(graph)
     for c in get_cycles(graph):
         print(c)
