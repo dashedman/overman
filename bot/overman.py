b9f137115dbcac82827e6bda2d43823078a8a4f0
dashedman <assarbsmail@gmail.com>
2023-07-05 09:35:44 +0300

    add trigger trade function

--- bot/overman.py
+++ bot/overman.py
@@ -19,6 +19,7 @@ import dto
 
 class Overman:
     graph: Graph
+    loop: asyncio.AbstractEventLoop
     tickers_to_pairs: dict[str, tuple[str, str]]
 
     def __init__(
@@ -30,8 +31,10 @@ class Overman:
         self.depth = depth
         self.prefix = prefix
         self.pivot_coins = pivot_coins
+
         self.order_book_by_ticker: dict[str, 'dto.BestOrders'] = {}
         self.__token = None
+        self.is_on_trade: bool = False
 
     @cached_property
     def logger(self):
@@ -70,6 +73,7 @@ class Overman:
     async def serve(self):
         # init graph
         self.logger.info('Loading graph')
+        self.loop = asyncio.get_running_loop()
         pairs = await self.load_graph()
         self.logger.info(
             'Loaded %s pairs, nodes: %s, edges: %s.',
@@ -89,6 +93,9 @@ class Overman:
             asyncio.create_task(self.monitor_socket(ch))
             for ch in ticker_chunks
         ]
+        tasks.append(
+            asyncio.create_task(self.status_monitor())
+        )
         await asyncio.gather(*tasks)
         # edit graph
         # trade if graph gave a signal
@@ -173,84 +180,51 @@ class Overman:
                 self.order_book_by_ticker[ticker].best_bid,
                 inverted=True,
             )
+            self.trigger_trade()
 
-            if not sum(edge.val != 0 for edge in self.graph.edges) < 200:
-                return
+    def trigger_trade(self):
+        if self.is_on_trade:
+            return
 
-            if random.random() < 0.99:
-                return 
-            # start_calc = time.time()
-            # profit = self.check_profit()
-            # end_calc = time.time()
-            # if profit:
-            #     profit_koef, cycle = profit
-            #     self.logger.info(
-            #         'Profit: %s, in time: %.3f, cycle: %s',
-            #         profit_koef, end_calc - start_calc, cycle
-            #     )
-            #
-            #     next_cycle = cycle.copy()
-            #     next_cycle.q.rotate(-1)
-            #     for index, ((node, edge), (next_node, _)) in enumerate(
-            #             zip(cycle, next_cycle), start=1):
-            #         print(index, node.value, edge.val, next_node.value)
+        self.is_on_trade = True
+        graph_copy = self.graph.copy()
+        asyncio.create_task(self.process_trade(graph_copy))
 
-            # start_calc = time.time()
-            # experimental_profit = self.check_profit_experimental()
-            # end_calc = time.time()
-            # if experimental_profit:
-            #     profit_koef, cycle = experimental_profit
-            #     self.logger.info(
-            #         'Experimental Profit: %s, validate_profit: %s, in time: %.3f, cycle: %s',
-            #         profit_koef, cycle.get_profit(),
-            #         end_calc - start_calc, cycle
-            #     )
-            #
-            #     next_cycle = cycle.copy()
-            #     next_cycle.q.rotate(-1)
-            #     for index, ((node, edge), (next_node, _)) in enumerate(zip(cycle, next_cycle), start=1):
-            #         print(index, node.value, edge.val, next_node.value)
+    async def process_trade(self, graph: Graph):
 
-            # start_calc = time.time()
-            # experimental_profit = self.check_profit_experimental_2()
-            # end_calc = time.time()
-            # if experimental_profit:
-            #     profit_koef, cycle = experimental_profit
-            #     self.logger.info(
-            #         'Experimental 2 Profit: %s, validate_profit: %s, in time: %.3f, cycle: %s',
-            #         profit_koef, cycle.get_profit(),
-            #         end_calc - start_calc, cycle
-            #     )
-            #
-            #     next_cycle = cycle.copy()
-            #     next_cycle.q.rotate(-1)
-            #     for index, ((node, edge), (next_node, _)) in enumerate(zip(cycle, next_cycle), start=1):
-            #         print(index, node.value, edge.val, next_node.value)
+        self.logger.info('Start trade')
 
-            start_calc = time.time()
-            experimental_profit = self.check_profit_experimental_3()
-            end_calc = time.time()
-            if experimental_profit:
-                profit_koef, cycle = experimental_profit
-                # profit_readable = 1 / profit_koef * 100 - 100
-                self.logger.info(
-                    'Experimental Profit: %.9f%%, validate_profit: %s, in time: %.3f, cycle: %s',
-                    profit_koef, cycle.get_profit(),
-                    end_calc - start_calc, cycle
-                )
+        start_calc = time.time()
+        experimental_profit = await self.loop.run_in_executor(
+            None,
+            self.check_profit_experimental_3,
+            graph
+        )
+        end_calc = time.time()
+        if experimental_profit:
+            profit_koef, cycle = experimental_profit
+            # profit_readable = 1 / profit_koef * 100 - 100
+            self.logger.info(
+                'Experimental Profit: %.9f%%, validate_profit: %s, in time: %.3f, cycle: %s',
+                profit_koef, cycle.get_profit(),
+                end_calc - start_calc, cycle
+            )
 
-                next_cycle = cycle.copy()
-                next_cycle.q.rotate(-1)
-                for index, ((node, edge), (next_node, _)) in enumerate(zip(cycle, next_cycle), start=1):
-                    print(index, node.value, edge.val, next_node.value)
+            next_cycle = cycle.copy()
+            next_cycle.q.rotate(-1)
+            for index, ((node, edge), (next_node, _)) in enumerate(zip(cycle, next_cycle), start=1):
+                print(index, node.value, edge.val, next_node.value)
 
-            start_calc = time.time()
-            profit = self.check_max_profit()
-            end_calc = time.time()
-            if profit:
-                profit_koef, cycle = profit
-                if profit_koef > 1:
-                    return
+        start_calc = time.time()
+        profit = await self.loop.run_in_executor(
+            None,
+            self.check_max_profit,
+            graph,
+        )
+        end_calc = time.time()
+        if profit:
+            profit_koef, cycle = profit
+            if profit_koef < 1:
                 # profit_readable = 1 / profit_koef * 100 - 100
                 self.logger.info(
                     'MAX Profit: %.9f%%, in time: %.3f, cycle: %s',
@@ -261,6 +235,9 @@ class Overman:
                 next_cycle.q.rotate(-1)
                 for index, ((node, edge), (next_node, _)) in enumerate(zip(cycle, next_cycle), start=1):
                     print(index, node.value, edge.val, next_node.value)
+
+        self.logger.info('End trade')
+        self.is_on_trade = False
 
     def update_graph(
             self,
@@ -299,17 +276,17 @@ class Overman:
         return None
 
     def check_max_profit(
-            self,
+            self, graph: Graph
     ) -> tuple[float, Cycle] | None:
         max_profit = 100000
         max_cycle = None
         for pivot_coin_index in self.pivot_indexes:
             for cycle in tqdm(
-                    self.graph.get_cycles(
+                    graph.get_cycles(
                     start=pivot_coin_index,
                     with_start=True,
-                    max_length=3,
-            ), ascii=True):
+                    max_length=4,
+            ), ascii=True, disable=True):
                 profit = cycle.get_profit()
                 if profit != 0 and profit < max_profit:
                     max_profit = profit
@@ -337,10 +314,10 @@ class Overman:
         return None
 
     def check_profit_experimental_3(
-            self,
+            self, graph: Graph,
     ) -> tuple[float, Cycle] | None:
         for pivot_coin_index in self.pivot_indexes:
-            profit, cycle = self.graph.get_profit_3(pivot_coin_index)
+            profit, cycle = graph.get_profit_3(pivot_coin_index)
             if cycle and not cycle.validate_cycle():
                 print('bad cycle')
                 return None
@@ -401,3 +378,16 @@ class Overman:
             if not edge.inversed
         ]
         return filtered_pairs
+
+    async def status_monitor(self):
+        while True:
+            counter = 0
+            index = 0
+            for index, edge in enumerate(self.graph.edges, start=1):
+                if edge.val != 0:
+                    counter += 1
+            self.logger.info(
+                'Status: %s of %s edgers filled. Asyncio tasks: %s',
+                counter, index, len(asyncio.all_tasks())
+            )
+            await asyncio.sleep(30)
